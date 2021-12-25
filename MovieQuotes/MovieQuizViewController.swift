b1d3a2ce5b4246bcdae5fcf106af8ffaa0a81c38
Linah-alkhurayyif <73877590+Linah-alkhurayyif@users.noreply.github.com>
2021-12-26 02:13:14 +0300

    Final

--- MovieQuotes/MovieQuizViewController.swift
+++ MovieQuotes/MovieQuizViewController.swift
@@ -3,30 +3,41 @@ import UIKit
 struct QuizMovieQuote{
     let movieName: String
     let quote: String
+    let image: String
 }
 
-class MovieQuizViewController: UIViewController {
+class MovieQuizViewController: UIViewController, UITextFieldDelegate {
     
 
-    @IBOutlet weak var guessMovieTextField: UITextField!
+    @IBOutlet weak var guessMovieTextField: RVS_AutofillTextField!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var guessButton: UIButton!
     @IBOutlet weak var quoteLabel: UILabel!
+    @IBOutlet weak var correctGuessImage: UIImageView!
+    @IBOutlet weak var nextButton: UIButton!
     
     var quiz_movies = [Movie]()
     var quotes = [QuizMovieQuote]()
     var totalScore = 0
     var currentQuote = 0
+    var MoviesNamesDictionary: [String] = [ "Call",
+                                                "Forgotten",
+                                                "Pandora",
+                                                "Sweet & Sour",
+                                                "The Flu",
+                                                "Train to Busan"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        guessMovieTextField?.dataSource = self
+        guessMovieTextField?.delegate = self
         setQuiz()
     }
     func setQuiz(){
         for movie in quiz_movies {
             if movie.isSelected{
                 for quote in movie.quotes{
-                    quotes.append(QuizMovieQuote(movieName: movie.name, quote: quote))
+                    quotes.append(QuizMovieQuote(movieName: movie.name, quote: quote,image: movie.image))
                 }
             }
            
@@ -50,10 +61,19 @@ class MovieQuizViewController: UIViewController {
             self.present(alert, animated: true, completion: nil)
         }else{
             if guessMovieTextField.text?.lowercased() == quotes[currentQuote].movieName.lowercased(){
-                totalScore = totalScore + 10
+                totalScore = totalScore + 1
                 scoreLabel.text = "Score: \(totalScore)"
                 guessMovieTextField.text = ""
-                nextMovie()
+                guessMovieTextField.isHidden = true
+                scoreLabel.isHidden = true
+                guessButton.isHidden = true
+                quoteLabel.isHidden = true
+               
+                correctGuessImage.image = UIImage(named: quotes[currentQuote].image)
+                
+                correctGuessImage.isHidden = false
+                nextButton.isHidden = false
+             
             }else{
                 let alert = UIAlertController(title: "Wrong Guess!!", message: "", preferredStyle: .alert)
                 alert.addAction(UIAlertAction(title: "OK", style: .default, handler:{action in
@@ -80,4 +100,52 @@ class MovieQuizViewController: UIViewController {
         }
     }
     
+    @IBAction func nextButton(_ sender: UIButton) {
+        guessMovieTextField.isHidden = false
+        scoreLabel.isHidden = false
+        guessButton.isHidden = false
+        quoteLabel.isHidden = false
+       
+        
+        correctGuessImage.isHidden = true
+        nextButton.isHidden = true
+        nextMovie()
+    }
+    
+}
+extension MovieQuizViewController: RVS_AutofillTextFieldDataSource {
+    /* ################################################################## */
+    /**
+     This is an Array of structs, that are the searchable data collection for the text field.
+     If this is empty, then no searches will return any results.
+     */
+    var textDictionary: [RVS_AutofillTextFieldDataSourceType] {
+        var index = 0
+        
+        let ret: [RVS_AutofillTextFieldDataSourceType] = MoviesNamesDictionary.compactMap {
+            let currentStr = $0.trimmingCharacters(in: .whitespacesAndNewlines)
+            if !currentStr.isEmpty {
+                defer { index += 1 }
+                return RVS_AutofillTextFieldDataSourceType(value: currentStr, refCon: index)
+            }
+            
+            return nil
+        }
+        
+        return ret
+    }
+}
+
+
+extension MovieQuizViewController: RVS_AutofillTextFieldDelegate {
+    /* ################################################################## */
+    /**
+     This is called when the user selects one of the autofill choices.
+     In this app, all we do is print to the debug console.
+     - parameter inAutofillTextField: The text field instance that the user affected.
+     - parameter selectionWasMade: The data item, with the string and the refCon.
+     */
+    func autoFillTextField(_ inAutofillTextField: RVS_AutofillTextField, selectionWasMade inSelectedItem: RVS_AutofillTextFieldDataSourceType) {
+        print("The user selected this: \(inSelectedItem.debugDescription)")
+    }
 }
